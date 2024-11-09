c2295b9b386b220ae8db01196d02cbde3c7c8a25
briskaanandaa <briskaananda103@gmail.com>
2024-11-09 12:52:26 +0700

    Slicing Update Profile Page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,12 +4,20 @@ import InputField from "../components/InputField";
 
 const Login = () => {
   return (
-    <div className="bg-white px-8 py-10 rounded-[30px] flex flex-col justify-center items-center">
+    <div className="bg-white px-10 py-10 rounded-[30px] flex flex-col justify-center items-center">
       <h1 className="text-slate-950 text-center text-3xl mb-3">
         <span className="text-blue-500 font-bold">VOCA</span>
         <br />
         Task
       </h1>
+      <InputField
+        label="Name"
+        type="text"
+        id="name"
+        name="name"
+        placeholder="Briska Ananda"
+        required={true}
+      />
       <InputField
         label="Email"
         type="email"
